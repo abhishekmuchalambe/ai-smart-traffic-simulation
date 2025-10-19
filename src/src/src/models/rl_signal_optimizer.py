f048b5a61622ee9bd370220bd9e327ba9f75274e
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:39:45 +0530

    Add RLSignalOptimizer model for traffic signal optimization

---
+++ src/src/src/models/rl_signal_optimizer.py
@@ -0,0 +1,7 @@
+class RLSignalOptimizer:
+    def __init__(self):
+        print("[MODEL] RLSignalOptimizer initialized")
+
+    def optimize(self, traffic_data):
+        print("[MODEL] Optimizing signal timings...")
+        return {"Junction-1": 45}
