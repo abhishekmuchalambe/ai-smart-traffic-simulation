a83d667b14d6411bd09aca9d72ed87a4d87a9ece
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:37:56 +0530

    Add TrafficFlowPredictor class for traffic flow prediction

---
+++ src/src/models/traffic_flow_predictor.py
@@ -0,0 +1,7 @@
+class TrafficFlowPredictor:
+    def __init__(self):
+        print("[MODEL] TrafficFlowPredictor initialized")
+
+    def predict(self, data):
+        print("[MODEL] Predicting traffic flow...")
+        return {"junction": "Junction-1", "predicted_density": "Moderate"}
