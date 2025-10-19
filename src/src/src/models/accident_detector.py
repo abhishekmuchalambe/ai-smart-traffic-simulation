396652a264d5a2abe3766bd26fd8c3b48a736b3e
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:38:49 +0530

    Add AccidentDetector class for accident detection

---
+++ src/src/src/models/accident_detector.py
@@ -0,0 +1,7 @@
+class AccidentDetector:
+    def __init__(self):
+        print("[MODEL] AccidentDetector initialized")
+
+    def detect(self, data):
+        print("[MODEL] Detecting accidents...")
+        return False
