0d046ea18a2cc53486543d2f1bf92da4147508c9
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:42:02 +0530

    Implement SimulatorAPI class with simulation method

---
+++ src/src/src/controller/src/simulation/simulator_api.py
@@ -0,0 +1,6 @@
+class SimulatorAPI:
+    def __init__(self):
+        print("[SIM] SimulatorAPI initialized")
+
+    def run_simulation(self, config):
+        print("[SIM] Running traffic simulation with config:", config)
