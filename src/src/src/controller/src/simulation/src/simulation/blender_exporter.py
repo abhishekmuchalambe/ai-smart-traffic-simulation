f334cde0f8a4c9990150816943c89af309c63a16
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:42:25 +0530

    Add BlenderExporter class for simulation export

---
+++ src/src/src/controller/src/simulation/src/simulation/blender_exporter.py
@@ -0,0 +1,6 @@
+class BlenderExporter:
+    def __init__(self):
+        print("[SIM] BlenderExporter initialized")
+
+    def export(self, simulation_data):
+        print("[SIM] Exporting simulation to Blender...")
