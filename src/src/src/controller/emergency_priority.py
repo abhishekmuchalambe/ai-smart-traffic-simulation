ba3ed30f6e496c1dcffdadcb69d95856101324d7
abhishekmuchalambe <abhishekmuchalambe17@gmail.com>
2025-10-19 16:41:34 +0530

    Add EmergencyPriority class for vehicle prioritization

---
+++ src/src/src/controller/emergency_priority.py
@@ -0,0 +1,6 @@
+class EmergencyPriority:
+    def __init__(self):
+        print("[CTRL] EmergencyPriority system online")
+
+    def prioritize(self, emergency_vehicle_data):
+        print("[CTRL] Giving priority to emergency vehicle...")
